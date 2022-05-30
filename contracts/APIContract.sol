338909ca945f94bf57f326279a9e2070caef79cd
Rafael450 <rafaelgamer450@gmail.com>
2022-05-30 14:22:57 -0300

    switched to kavan testnet

--- contracts/APIContract.sol
+++ contracts/APIContract.sol
@@ -23,9 +23,9 @@ contract APIConsumer is ChainlinkClient, ConfirmedOwner{
     event RequestVolume(bytes32 indexed requestId, uint256 volume);
 
     constructor() ConfirmedOwner(msg.sender){
-        setChainlinkToken(0x01BE23585060835E02B77ef475b0Cc51aA1e0709);
+        setChainlinkToken(0xa36085F69e2889c224210F603D836748e7dC0088);
         setChainlinkOracle(0xF59646024204a733E1E4f66B303c9eF4f68324cC);
-        jobId = '6a92925dbb0e48e9b375b1deac4751c0';
+        jobId = '50d7bca4dcf7462a83e7282b62646466';
         fee = (1 * LINK_DIVISIBILITY) * 1 / 10; // 0,1 * 10**18 (Varies by network and job)
     }
 
@@ -37,8 +37,7 @@ contract APIConsumer is ChainlinkClient, ConfirmedOwner{
         Chainlink.Request memory req = buildChainlinkRequest(jobId, address(this), this.fulfill.selector);
 
         // Set the URL to perform the GET request on
-        req.add('get', 'http://www.randomnumberapi.com/api/v1.0/random?min=100&max=1000');
-        req.add('path', '0');
+        req.add('get', 'https://min-api.cryptocompare.com/data/pricemultifull?fsyms=ETH&tsyms=USD');
 
         // Set the path to find the desired data in the API response, where the response format is:
         // {"RAW":
@@ -51,6 +50,13 @@ contract APIConsumer is ChainlinkClient, ConfirmedOwner{
         //   }
         //  }
         // request.add("path", "RAW.ETH.USD.VOLUME24HOUR"); // Chainlink nodes prior to 1.0.0 support this format
+        req.add('path', 'RAW,ETH,USD,VOLUME24HOUR'); // Chainlink nodes 1.0.0 and later support this format
+
+        // Multiply the result by 1000000000000000000 to remove decimals
+        int256 timesAmount = 10**18;
+        req.addInt('times', timesAmount);
+
+        // Sends the request
         return sendChainlinkRequest(req, fee);
     }
 
@@ -60,6 +66,7 @@ contract APIConsumer is ChainlinkClient, ConfirmedOwner{
     function fulfill(bytes32 _requestId, uint256 _number) public recordChainlinkFulfillment(_requestId) {
         emit RequestVolume(_requestId, _number);
         number = _number;
+
     }
 }
 
